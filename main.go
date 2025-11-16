43e966362a2a7fbc66e3447a1133ea11bfd670b3
kigongovincent <kigongovincent81@gmail.com>
2025-11-16 21:32:39 +0300

    init project

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello world</h1>")
+	})
+
+	http.ListenAndServe(":80", nil)
+}
