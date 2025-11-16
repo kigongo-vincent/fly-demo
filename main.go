e156173c9c8263880e8429710d730e78c014ceb7
kigongovincent <kigongovincent81@gmail.com>
2025-11-17 00:24:17 +0300

    deployed

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	PORT := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Hello world</h1>")
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":"+PORT, nil)
 }
